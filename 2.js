989273c51add88f817b86fbf3d3ba1fa9332a3d9
arenasoy <caro.arenas2607@gmail.com>
2020-06-23 22:19:50 -0300

    day 2 - part 2

--- 2.js
+++ 2.js
@@ -7,8 +7,11 @@ const rl = readline.createInterface({
 
 let array = []
 
+//part 1
+
+/*
 rl.on('line', (intcode) => {
-	array = intcode.split(',');
+	original = intcode.split(',');
 
 	array[1] = 12;
 	array[2] = 2;
@@ -23,4 +26,35 @@ rl.on('line', (intcode) => {
 	}
 
 	console.log(array[0]);
+	console.log(original[0]);
 });
+*/
+
+
+//part 2
+rl.on('line', (intcode) => {
+	original = intcode.split(',');
+
+	for (let noun = 0; noun < 100; noun++) {
+		for (let verb = 0; verb < 100; verb++) {
+			let array = [...original]
+			
+			array[1] = noun;
+			array[2] = verb;
+			for (let i = 0; i < array.length; i += 4) {
+				if (array[i] == 1) {
+					array[array[i + 3]] = parseInt(array[array[i + 1]]) + parseInt(array[array[i + 2]]);
+				} else if (array[i] == 2) {
+					array[array[i + 3]] = parseInt(array[array[i + 1]]) * parseInt(array[array[i + 2]]);
+				} else if (array[i] == 99) {
+					break;
+				}
+			}
+
+			if (array[0] == 19690720) {
+				console.log(100 * parseInt(noun) + parseInt(verb));
+				return;
+			}
+		}
+	}
+}); 
